fc369ff20e85d236a922b2122a68b8e0d50af44f
Josh Oram <josh.oram80@gmail.com>
2015-06-12 16:30:39 +1000

    Added fbmovies Filebot alias

--- .zsh/aliases.zsh
+++ .zsh/aliases.zsh
@@ -78,6 +78,8 @@ alias sz='source ~/.zshrc'
 
 #Filebot aliases
 alias fbtv='filebot -rename ./**/* --db TheTVDB -non-strict --format "/home/stuntard/Videos/TV/{n}/Season {s.pad(2)}/{n}.{s00e00}.{t}"'
+alias fbmovies='filebot -rename ./**/* --db TheMovieDB -non-strict --format "/home/stuntard/Videos/Movies/{n}/{n}{t}"' 
+
 
 # FTP Servers
 alias seedbox='set sftp:connect-program "ssh -a -x -i ~/.ssh/id_dsa"; lftp -u stuntard, sftp://spicywiener.chmuranet.net'
